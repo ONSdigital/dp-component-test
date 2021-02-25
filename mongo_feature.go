b49719db54ff38b71ef1b7f009c3626de05a57e1
Timothy Long <tim_long123@hotmail.com>
2021-02-25 11:21:07 +0000

    increase timeout to 2 mins

--- mongo_feature.go
+++ mongo_feature.go
@@ -33,7 +33,7 @@ func NewMongoFeature(mongoOptions MongoOptions) *MongoFeature {
 	opts := memongo.Options{
 		Port:           mongoOptions.Port,
 		MongoVersion:   mongoOptions.MongoVersion,
-		StartupTimeout: 10 * time.Second,
+		StartupTimeout: 2 * time.Minute,
 		DownloadURL:    mongoOptions.DownloadURL,
 	}
 
