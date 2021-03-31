83cc63c8d40a12d1da665981d0e7e9292dace5ae
Jon Acker <jon.acker@yoyo.org>
2021-03-31 14:43:20 +0100

    Update ErrorFeature to include testing.TB required to be able to pass off this struct as a testing.T in all cases

--- error_feature.go
+++ error_feature.go
@@ -1,15 +1,32 @@
 package componenttest
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 type ErrorFeature struct {
+	testing.TB
+	// composing testing.TB allows ErrorFeature to be passed off as a testing.T thingy
 	err error
 }
 
-func (f *ErrorFeature) Errorf(format string, args ...interface{}) {
-	f.err = fmt.Errorf(format, args...)
+func (t *ErrorFeature) Log(args ...interface{}) {
 }
 
-func (f *ErrorFeature) StepError() error {
-	return f.err
+func (t *ErrorFeature) Logf(format string, args ...interface{}) {
+	t.err = fmt.Errorf(format, args...)
 }
+
+func (t *ErrorFeature) Errorf(format string, args ...interface{}) {
+	t.err = fmt.Errorf(format, args...)
+}
+
+func (t *ErrorFeature) StepError() error {
+	return t.err
+}
+func (t *ErrorFeature) Helper() {}
+func (t *ErrorFeature) Name() string {
+	return "Component Test"
+}
+
