8e47f6108eee00f5c94674442a89180048afbb09
Timothy Long <tim_long123@hotmail.com>
2021-04-06 11:32:54 +0100

    update script command

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -2,9 +2,9 @@
 
 cwd=$(pwd)
 
-pushd $cwd/dp-dataset-api
+pushd $cwd/dp-component-test
 #  This is required to tell memongo which binary to download, without this
 #  memongo tries to download a binary for debian which doesn't work on the container
   export MEMONGO_DOWNLOAD_URL=https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.0.23.tgz
-  make test-component
+  make test
 popd
