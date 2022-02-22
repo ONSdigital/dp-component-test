39ef22febfba7d55f816d3ed797d3baf0f95e930
Pritesh Patel <40333998+p-patel@users.noreply.github.com>
2022-02-22 08:30:03 +0000

    Use newly created chrome-go image for component job.
    Also update component job script to remove manual chrome installation

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -eux
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-apt-get update && apt-get -y install google-chrome-stable
-
 cwd=$(pwd)
+
 pushd $cwd/dp-component-test
   make test-component
 popd
